c1a69aef17931e271002e4cf5b9da91aac7b4096
car eat fish <18434367522@163.com>
2018-11-24 17:50:27 +0800

    d

---
+++ react-boke/dist/precache-manifest.d8a97a83898d0ae1ec0e6ac70ed5a825.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1808121fb9d28dc3c7c821523115c3e7",
+    "url": "./static/media/weixin.1808121f.jpg"
+  },
+  {
+    "revision": "85aed8f2d35d9c24e784c68647a76f8a",
+    "url": "./static/media/logo.85aed8f2.jpg"
+  },
+  {
+    "revision": "6c97be2bee42e1f965761376c01453fa",
+    "url": "./static/media/load.6c97be2b.gif"
+  },
+  {
+    "revision": "165d9003dbb4ffcc27870852c6feeb5a",
+    "url": "./static/media/banner.165d9003.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6657285e285cbc65efef",
+    "url": "./static/js/main.6657285e.chunk.js"
+  },
+  {
+    "revision": "7b7570cbabcb06ad562b",
+    "url": "./static/js/1.7b7570cb.chunk.js"
+  },
+  {
+    "revision": "6657285e285cbc65efef",
+    "url": "./static/css/main.18648445.chunk.css"
+  },
+  {
+    "revision": "6aee7a48f3260a8bd2dc5a660aa9af28",
+    "url": "./index.html"
+  }
+];
